b6ec9d4990148abfde3bb0948bbe3958a2cf8364
User <User@DESKTOP-M0S4FI9>
2026-03-24 13:46:00 -0500

    .

---
+++ SportsLeague.API/DTOs/Request/UpdateStatusDTO.cs
@@ -0,0 +1,13 @@
+﻿using SportsLeague.Domain.Enums;
+
+
+namespace SportsLeague.API.DTOs.Request;
+
+
+public class UpdateStatusDTO
+
+{
+
+    public TournamentStatus Status { get; set; }
+
+}
--- SportsLeague.API/Mappings/MappingProfile.cs
+++ SportsLeague.API/Mappings/MappingProfile.cs
@@ -37,6 +37,27 @@ public class MappingProfile : Profile
 
         opt => opt.MapFrom(src => src.Team.Name));
 
+        // Referee mappings
+
+        CreateMap<RefereeRequestDTO, Referee>();
+
+        CreateMap<Referee, RefereeResponseDTO>();
+
+
+        // Tournament mappings
+
+        CreateMap<TournamentRequestDTO, Tournament>();
+
+        CreateMap<Tournament, TournamentResponseDTO>()
+
+        .ForMember(
+
+        dest => dest.TeamsCount,
+
+        opt => opt.MapFrom(src =>
+
+        src.TournamentTeams != null ? src.TournamentTeams.Count : 0));
+
     }
 
 }
