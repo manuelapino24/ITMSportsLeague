8fde52f271e2382bc1308c6cf5d91af78b78e933
User <User@DESKTOP-M0S4FI9>
2026-03-26 13:39:40 -0500

    add Sponsor and TournamentSponsor entities with enum and interfaces

---
+++ SportsLeague.Domain/Enums/SponsorCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace SportsLeague.Domain.Enums;
+
+public enum SponsorCategory
+{
+    Main = 0,    // main sponsor (Patrocinador principal)
+    Gold = 1,    // gold (Oro)
+    Silver = 2,  // silver (Plata) 
+    Bronze = 3   // bronze(Bronce)
+}
